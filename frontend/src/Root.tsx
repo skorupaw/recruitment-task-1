1cbfcea16e78c0e2ad9913793e5948593ae9075e
Wojciech <wojciech.skorupa@pragmasoft.pl>
2024-03-10 23:02:43 +0100

    Update `Root` component in `/src`

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,14 +1,20 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import Mood from "./components/Mood";
 
-const client = new ApolloClient({
-  uri: "http://localhost:5173/graphql",
-  cache: new InMemoryCache(),
-});
+export const apolloClient = () => {
+  return new ApolloClient({
+    uri: "http://localhost:5173/graphql",
+    cache: new InMemoryCache(),
+  });
+};
 
-const router = createBrowserRouter([
+const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
@@ -19,12 +25,12 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+];
 
 export default function Root() {
   return (
-    <ApolloProvider client={client}>
-      <RouterProvider router={router} />
+    <ApolloProvider client={apolloClient()}>
+      <RouterProvider router={createBrowserRouter(routes)} />
     </ApolloProvider>
   );
 }
