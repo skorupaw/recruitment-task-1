5426e9200e73cb4b27b33ed175a521f97d9545ff
Wojciech <wojciech.skorupa@pragmasoft.pl>
2024-03-10 23:01:08 +0100

    Update `test` config in `vite.config.ts`

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: "./setup-tests.ts",
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
