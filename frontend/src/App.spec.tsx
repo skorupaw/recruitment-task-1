0a375b298f1bfeaf0710d83d535a72a9e2b24fe6
Wojciech <wojciech.skorupa@pragmasoft.pl>
2024-03-10 23:03:13 +0100

    Update example unit-tests

--- frontend/src/App.spec.tsx
+++ frontend/src/App.spec.tsx
@@ -1,1 +1,11 @@
-describe.skip("App", () => {});
+import { render, screen } from "./test-utils/testing-library";
+import App from "./App";
+
+describe("App", () => {
+  it("focuses the search input", async () => {
+    render(<App />);
+
+    expect(await screen.findByPlaceholderText(/Search/i)).toHaveFocus();
+  });
+  it.todo('prevents user from selecting more then 3 "mood cards"');
+});
--- frontend/src/components/Mood.spec.tsx
+++ frontend/src/components/Mood.spec.tsx
@@ -1,22 +1,24 @@
-import { render, screen } from "@testing-library/react";
+import { Route, Routes } from "react-router-dom";
+import { render, screen } from "../test-utils/testing-library";
 import Mood from "./Mood";
-import { BrowserRouter } from "react-router-dom";
-
-vi.mock("react-router-dom", async () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const actual = (await vi.importActual("react-router-dom")) as any;
-  return {
-    ...actual,
-    useParams: () => ({ moodId: 1 }),
-  };
-});
 
 describe("Mood", () => {
-  it("renders mood details", async () => {
-    render(<Mood />, { wrapper: BrowserRouter });
+  test("mood details are being rendered", async () => {
+    setup();
 
     expect(
       await screen.findByRole("heading", { name: /Happiness/ }),
     ).toBeInTheDocument();
   });
 });
+
+const setup = () =>
+  render(
+    <Routes>
+      <Route path="/" element={<Empty />} />
+      <Route path="/mood/:moodId" element={<Mood />} />
+    </Routes>,
+    { routes: ["/mood/1"] },
+  );
+
+const Empty = () => <div />;
