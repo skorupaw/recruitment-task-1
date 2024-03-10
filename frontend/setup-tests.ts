1d69842f140605f0c349ad672cd2d9f9acd61e52
Wojciech <wojciech.skorupa@pragmasoft.pl>
2024-03-10 23:02:12 +0100

    Update `setup-tests.ts`

--- frontend/setup-tests.ts
+++ frontend/setup-tests.ts
@@ -1,28 +1,6 @@
-import "vitest-dom/extend-expect";
+import "@testing-library/jest-dom";
 
-import { rest } from "msw";
-import { setupServer } from "msw/node";
-
-const server = setupServer(
-  rest.get("http://localhost:5173/api/moods/1", (req, res, ctx) => {
-    return res(
-      ctx.json({
-        id: 1,
-        title: "Happiness",
-        emoji: "😄",
-        description: "A state of being happy or experiencing pleasure.",
-        word: {
-          partOfSpeech: "Noun",
-          definitions: [
-            "The state of being happy.",
-            "A feeling of pleasure or contentment.",
-          ],
-          pronunciation: "/ˈhæp.i.nəs/",
-        },
-      }),
-    );
-  }),
-);
+import { server } from "./src/test-utils/msw";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
