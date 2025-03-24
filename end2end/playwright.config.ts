ec1aaa3f86144e261c76f2a8483f71fff798ee31
skorupaw <skorupa@corporater.com>
2025-03-24 09:32:17 +0100

    Update Playwright reporter configuration for better test output

--- end2end/playwright.config.ts
+++ end2end/playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "html",
+  reporter: [["list"], ["html"], process.env.CI ? ["github"] : ["null"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
